d444d4312d7ed8d45e5d4a373fdc335e5acbe197
atungare <ajay.tungare@gmail.com>
2016-04-23 16:22:08 -0700

    js curried

--- Y.js
+++ Y.js
@@ -14,10 +14,12 @@ var workingFact = Y(almostFactorial)
 console.log(workingFact(10));
 
 function partFactorial (self) {
-  function h (f, n) {
-    return (n === 0) ? 1 : (n * f(n - 1));
+  function h (f) {
+    return function (n) {
+      return (n === 0) ? 1 : (n * f(n - 1));
+    };
   }
-  return function (x) { return h(self(self), x) };
+  return function (x) { return h(self(self))(x); };
 }
 
 var factorial = partFactorial(partFactorial);
