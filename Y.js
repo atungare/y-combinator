51cf782f81113b35d3a721dc50090b00583fbad0
atungare <ajay.tungare@gmail.com>
2016-04-23 16:24:01 -0700

    basic substitution

--- Y.js
+++ Y.js
@@ -14,11 +14,7 @@ var workingFact = Y(almostFactorial)
 console.log(workingFact(10));
 
 function partFactorial (self) {
-  function h (f) {
-    return function (n) {
-      return (n === 0) ? 1 : (n * f(n - 1));
-    };
-  }
+  var h = almostFactorial;
   return function (x) { return h(self(self))(x); };
 }
 
